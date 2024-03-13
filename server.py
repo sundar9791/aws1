2a0d16b81f31df025d84df541abb44141763816e
sundar9791 <34347281+sundar9791@users.noreply.github.com>
2024-03-12 21:14:21 -0400

    Update server.py

--- server.py
+++ server.py
@@ -3,6 +3,13 @@ from pyramid.config import Configurator
 from pyramid.response import Response
 import os
 
+def hello_world_test(request):
+    name = os.environ.get('NAME')
+    if name == None or len(name) == 0:
+        name = "world"
+    message = "Hello, " + name + " test!\n"
+    return Response(message)
+    
 def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
@@ -15,6 +22,8 @@ if __name__ == '__main__':
     with Configurator() as config:
         config.add_route('hello', '/')
         config.add_view(hello_world, route_name='hello')
+        config.add_route('hello_test', '/test')
+        config.add_view(hello_world_test, route_name='hello_test')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', port, app)
     server.serve_forever()
